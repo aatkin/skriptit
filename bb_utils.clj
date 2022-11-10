a3a38810f83c6c952719b76c04d4993e73c150ae
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2022-11-10 11:22:09 +0200

    feat: save current version of chromedriver

--- bb_utils.clj
+++ bb_utils.clj
@@ -9,16 +9,17 @@
     (-> (partial println "DEBUG:")
         (apply xs))))
 
-(defn unzip-bytes [{:keys [data filename]}]
+(defn unzip-bytes [{:keys [data]}]
   (let [temp-dir (fs/create-temp-dir)
-        path (fs/path temp-dir filename)]
-    (fs/write-bytes path data)
-    (debug-println "wrote bytes to" (str path))
-    (fs/unzip (fs/file path) temp-dir)
-    (let [files (->> (fs/list-dir temp-dir)
-                     ; trim leading temp dir paths
-                     (map #(clojure.string/replace-first (str %) (str temp-dir "/") ""))
-                     (remove #{filename}))]
-      (debug-println "unzipped files" files)
-      {:dir temp-dir
-       :file-list files})))
+        filename (str "unzip_" (System/currentTimeMillis))
+        path (fs/path temp-dir filename)
+        _ (fs/write-bytes path data)
+        _ (debug-println "wrote bytes to" (str path))
+        _ (fs/unzip (fs/file path) temp-dir)
+        files (->> (fs/list-dir temp-dir)
+                   ; trim leading temp dir paths
+                   (map #(clojure.string/replace-first (str %) (str temp-dir "/") ""))
+                   (remove #{filename}))
+        _ (debug-println "unzipped files" files)]
+    {:dir temp-dir
+     :file-list files}))
--- update_chromedriver.clj
+++ update_chromedriver.clj
@@ -7,26 +7,31 @@
 ;; https://book.babashka.org/#_parsing_command_line_arguments
 (def cli-options [["-d" "--debug" "Debug mode"]])
 (def opts (:options (parse-opts *command-line-args* cli-options)))
+(def version-file "chromedriver_version.txt")
+(def current-version (when (.exists (fs/file version-file))
+                       (slurp version-file)))
 
 (binding [bb-utils/*debug* (:debug opts)]
   (bb-utils/debug-println ::start)
 
   (def version (:body (curl/get "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")))
+  (spit version-file version)
   (def uri (str "https://chromedriver.storage.googleapis.com/" version "/chromedriver_mac64.zip"))
 
-  (println "latest chromedriver version:" version)
-  (println "NB: this will overwrite your current file in /usr/local/bin/chromedriver!")
-  (println "do you want to proceed? (y/n)")
-
-  (when (some #{"y" "yes"} (list (read-line)))
-    (def response (curl/get uri {:as :bytes}))
-    (bb-utils/debug-println "downloaded file:" response)
-    (def files (bb-utils/unzip-bytes {:data (:body response)
-                                      :filename "chromedriver_latest.zip"}))
-    (assert (some #{"chromedriver"} (:file-list files))
-            "unzipped contents must contain chromedriver")
-    (-> (fs/file (:dir files) "chromedriver")
-        (fs/copy "/usr/local/bin" {:replace-existing true}))
-    (bb-utils/debug-println "wrote to" (str (fs/path "/usr/local/bin" "chromedriver"))))
+  (if (= current-version version)
+    (println "already on latest version:" version)
+    (do
+      (println "latest chromedriver version:" version)
+      (println "NB: this will overwrite your current file in /usr/local/bin/chromedriver!")
+      (println "do you want to proceed? (y/n)")
+      (when (some #{"y" "yes"} (list (read-line)))
+        (let [response (curl/get uri {:as :bytes})
+              _ (bb-utils/debug-println "downloaded file:" response)
+              files (bb-utils/unzip-bytes {:data (:body response)})]
+          (assert (some #{"chromedriver"} (:file-list files))
+                  "unzipped contents must contain chromedriver")
+          (-> (fs/file (:dir files) "chromedriver")
+              (fs/copy "/usr/local/bin" {:replace-existing true}))
+          (bb-utils/debug-println "wrote to" (str (fs/path "/usr/local/bin" "chromedriver")))))))
 
   (bb-utils/debug-println ::end))
