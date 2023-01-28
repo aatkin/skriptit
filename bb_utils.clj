a9966be17633a06ba7b43028b892339c486f7e10
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2023-01-28 12:52:16 +0200

    refactor: create more fs utils in bb-utils namespace, add force flag to update-chromedriver

--- bb_utils.clj
+++ bb_utils.clj
@@ -1,6 +1,6 @@
 (ns bb-utils
   (:require [babashka.fs :as fs]
-            [clojure.string]))
+            [clojure.string :as str]))
 
 (def ^:dynamic *debug*)
 
@@ -11,20 +11,35 @@
 
 (defn debug-println [& xs]
   (when (and (bound? #'*debug*) *debug*)
-    (-> (partial println "DEBUG:")
-        (apply xs))))
+    (apply println "DEBUG:" xs)))
+
+(defn write-to-file
+  "Create temp dir and write data to filename in temp dir. Returns File."
+  [data filename]
+  (let [temp-dir (fs/create-temp-dir)
+        path (fs/path temp-dir filename)]
+    (fs/write-bytes path data)
+    (debug-println #'write-to-file "wrote bytes to" (str path))
+    (fs/file path)))
+
+(defn get-file-list [dir]
+  (let [+dir-path+ (str dir "/")
+        to-file-path #(-> % str (str/replace-first +dir-path+ ""))]
+    (->> (fs/list-dir dir)
+         (into #{} (map to-file-path)))))
 
 (defn unzip-bytes [{:keys [data]}]
-  (let [temp-dir (fs/create-temp-dir)
-        filename (str "unzip_" (System/currentTimeMillis))
-        path (fs/path temp-dir filename)
-        _ (fs/write-bytes path data)
-        _ (debug-println "wrote bytes to" (str path))
-        _ (fs/unzip (fs/file path) temp-dir)
-        files (->> (fs/list-dir temp-dir)
-                   ; trim leading temp dir paths
-                   (map #(clojure.string/replace-first (str %) (str temp-dir "/") ""))
-                   (remove #{filename}))
-        _ (debug-println "unzipped files" files)]
-    {:dir temp-dir
-     :file-list files}))
+  (let [temp-dir (fs/create-temp-dir)]
+    (-> data
+        (write-to-file (str "unzip_" (System/currentTimeMillis)))
+        (fs/unzip temp-dir))
+    (let [file-list (get-file-list temp-dir)]
+      (debug-println #'unzip-bytes "unzipped files" file-list)
+      {:dir temp-dir
+       :file-list file-list})))
+
+(defn slurp-if-exists [filename]
+  (let [maybe-file (relative-file filename)]
+    (when (.exists maybe-file)
+      (slurp maybe-file))))
+
--- update_chromedriver.clj
+++ update_chromedriver.clj
@@ -1,37 +1,55 @@
+#!/usr/bin/env bb
+
 (ns update-chromedriver
-  (:require [bb-utils :as bb-utils]
+  (:require [bb-utils :as bbu]
             [babashka.curl :as curl]
             [babashka.fs :as fs]
             [clojure.tools.cli :refer [parse-opts]]))
 
+(def +version-file+ "chromedriver_version.txt")
+
 ;; https://book.babashka.org/#_parsing_command_line_arguments
-(def cli-options [["-d" "--debug" "Debug mode"]])
+(def cli-options [["-d" "--debug" "Debug mode"]
+                  ["-f" "--force" "Force update"]])
 (def opts (:options (parse-opts *command-line-args* cli-options)))
-(def version-file (bb-utils/relative-file "chromedriver_version.txt"))
-(def current-version (when (.exists version-file)
-                       (slurp version-file)))
 
-(binding [bb-utils/*debug* (:debug opts)]
-  (bb-utils/debug-println ::start)
+(defn update-version-file [version]
+  (-> (bbu/relative-file +version-file+)
+      (spit version)))
 
-  (def version (:body (curl/get "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")))
-  (def uri (str "https://chromedriver.storage.googleapis.com/" version "/chromedriver_mac64.zip"))
+(defn get-chromedriver [version & [{:keys [os]}]]
+  (let [os-suffix (-> {:mac/intel "/chromedriver_mac64.zip"}
+                      (get os))
+        uri (str "https://chromedriver.storage.googleapis.com/" version os-suffix)]
+    (bbu/debug-println #'get-chromedriver "get uri" uri)
+    (let [response (curl/get uri {:as :bytes})]
+      (bbu/debug-println #'get-chromedriver "downloaded file:" response)
+      (let [files (bbu/unzip-bytes {:data (:body response)})]
+        (assert (some #{"chromedriver"} (:file-list files))
+                "unzipped contents must contain chromedriver")
+        files))))
 
-  (if (= current-version version)
-    (println "already on latest version:" version)
-    (do
-      (println "latest chromedriver version:" version (str "(current: " current-version ")"))
-      (println "NB: this will overwrite your current file in /usr/local/bin/chromedriver!")
-      (println "do you want to proceed? (y/n)")
-      (when (some #{"y" "yes"} (list (read-line)))
-        (let [response (curl/get uri {:as :bytes})
-              _ (bb-utils/debug-println "downloaded file:" response)
-              files (bb-utils/unzip-bytes {:data (:body response)})]
-          (assert (some #{"chromedriver"} (:file-list files))
-                  "unzipped contents must contain chromedriver")
-          (-> (fs/file (:dir files) "chromedriver")
-              (fs/copy "/usr/local/bin" {:replace-existing true}))
-          (bb-utils/debug-println "wrote to" (str (fs/path "/usr/local/bin" "chromedriver")))
-          (spit version-file version)))))
+(defn main []
+  (binding [bbu/*debug* (:debug opts)]
+    (bbu/debug-println #'main :main/start)
 
-  (bb-utils/debug-println ::end))
+    (let [current-version (bbu/slurp-if-exists "chromedriver_version.txt")
+          version (:body (curl/get "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"))]
+      (if (and (not (:force opts))
+               (= current-version version))
+        (println "already on latest version:" version)
+        (do
+          (println "latest chromedriver version:" version (str "(current: " current-version ")"))
+          (println "NB: this will overwrite your current file in /usr/local/bin/chromedriver!")
+          (println "do you want to proceed? (y/n)")
+          (when (some #{"y" "yes"} (list (read-line)))
+            (-> (get-chromedriver version {:os :mac/intel})
+                (:dir) ; temp dir path
+                (fs/file "chromedriver")
+                (fs/copy "/usr/local/bin" {:replace-existing true}))
+            (bbu/debug-println #'main "wrote to" (str (fs/path "/usr/local/bin" "chromedriver")))
+            (update-version-file version)))))
+
+    (bbu/debug-println #'main :main/end)))
+
+(main)
