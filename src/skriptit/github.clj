cdba5ab9d06825e1bf3900d3d934587102639c4f
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2024-08-29 11:55:29 +0300

    refactor: extract gh template functions

--- src/skriptit/github.clj
+++ src/skriptit/github.clj
@@ -3,21 +3,26 @@
             [clojure.pprint]
             [clojure.string :as str]))
 
-(defn wrap-vec [s] (str "[" s "]"))
-(defn wrap-parens [s] (str "(" s ")"))
-
 (defn md-url [& s]
-  (str (wrap-vec (apply str s))
-       (wrap-parens "{{.url}}")))
+  (str "[" (str/join "" s) "]" "({{.url}})"))
 
-(defn template-if [condition then & [else]]
-  (str "{{if " condition "}}"
-       then
-       else
-       "{{end}}"))
+(defn t-if
+  "Template if-else"
+  [condition then & [else]]
+  (let [then-else (->> [then else]
+                       (remove str/blank?)
+                       (str/join " "))]
+    (str "{{if" condition "}}" then-else "{{end}}")))
 
-(defn template-pipe [p1 p2 & pipes]
+(defn t-pipe
+  "Template pipe"
+  [p1 p2 & pipes]
   (str "{{" (str/join " | " (concat [p1 p2] pipes)) "}}"))
+
+(defn join-labels-by-name [sep]
+  (t-pipe ".labels"
+          "pluck \"name\""
+          (str "join \"" sep "\"")))
 
 (defn view-issue
   "View Github issue as markdown link `[.title .issue-number (.labels)](.url)`"
@@ -29,9 +34,8 @@
                      "gh issue view" issue-number
                      "--json" "labels,number,title,url"
                      "--template" (md-url "{{.title}} #{{.number}}"
-                                          (template-if ".labels"
-                                                       (str " " (wrap-parens (str "labels: "
-                                                                                  (template-pipe ".labels" "pluck \"name\"" "join \", \"")))))))
+                                          (t-if ".labels"
+                                                (str " (labels: " (join-labels-by-name ", ") ")"))))
               :out
               str/split-lines
               first)]
@@ -46,7 +50,8 @@
   (let [s (-> (shell {:out :string}
                      "gh pr view" pr-number
                      "--json" "author,labels,number,title,url"
-                     "--template" (md-url "{{.title}} #{{.number}} " (wrap-parens "by: {{.author.login}}")))
+                     "--template" (md-url "{{.title}} #{{.number}}"
+                                          "(by: {{.author.login}})"))
               :out
               str/split-lines
               first)]
